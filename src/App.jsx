a9a4f3eec96bf28b4e01d80a7aa10c1bdda71d13
Jonathan Warner <jewarner57@gmail.com>
2023-07-09 14:27:21 -0700

    add icons to new character modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-// - should be the codename, 
-// - should be able to delete character on opera browser, 
+// debug: characters are not overwriting correctly on opera browser
 // - write up some instructions for how to use it that appear at first start
 // or can be accessed view a question mark. 
-// Confirm file upload before upload and close upload modal after upload
 
 import './App.css';
 import { useState, useEffect, useCallback, useRef } from 'react';
@@ -71,7 +69,9 @@ function App() {
   }
 
   const createNewCharacter = () => {
-    loadCharacterFromID(`spygame-${uuidv4()}`);
+    const id = `spygame-${uuidv4()}`
+    loadCharacterFromID(id);
+    saveCharacterToLocalStorage(id, { characterID: currentCharacterID })
   }
 
   const saveCharacterToLocalStorage = (id, characterObject) => {
@@ -112,11 +112,13 @@ function App() {
       modalOpen && <CustomModal closeModal={() => setModalOpen(false)}>
         <div className='new-character-selector'>
           <div className='new-character-upload' onClick={(e) => handleUploadClick(e)}>
-            <p>Upload character file</p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M10 9h-6l8-9 8 9h-6v11h-4v-11zm11 11v2h-18v-2h-2v4h22v-4h-2z" /></svg>
+            Upload character file
             <input ref={hiddenFileInput} type="file" style={{ display: 'none' }} onChange={(e) => { handleUploadChange(e); setModalOpen(false) }} />
           </div>
-            <div className='new-character-blank' onClick={() => {createNewCharacter(); setModalOpen(false);}}>
-            Create new blank character
+          <div className='new-character-blank' onClick={() => {createNewCharacter(); setModalOpen(false);}}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill='currentColor' viewBox="0 0 24 24"><path d="M18.5 15c-2.483 0-4.5 2.015-4.5 4.5s2.017 4.5 4.5 4.5 4.5-2.015 4.5-4.5-2.017-4.5-4.5-4.5zm2.5 5h-2v2h-1v-2h-2v-1h2v-2h1v2h2v1zm-7.18 4h-12.82v-24h10.189c3.163 0 9.811 7.223 9.811 9.614v3.887c-.624-.261-1.297-.422-2-.476v-2.569c0-4.106-6-2.456-6-2.456s1.518-6-2.638-6h-7.362v20h9.501c.313.749.765 1.424 1.319 2z" /></svg>
+            New blank character
           </div>
         </div>
       </CustomModal>
