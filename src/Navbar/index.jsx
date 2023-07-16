1bdf7e66a38d9a9fb7e6da6de53ecc45efef0560
Jonathan Warner <jewarner57@gmail.com>
2023-07-16 14:25:08 -0700

    add help modal

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+import CustomModal from '../CustomModal';
 import './index.css';
 
 function Navbar({ downloadJson, uploadJson, characterList, currentCharacter, setCharacter, createNewCharacter }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const hasViewedHelp = localStorage.getItem('viewed-help')
+  const setViewedHelp = () => {
+    localStorage.setItem('viewed-help', true)
+  }
 
   return (
     <div className="navbar">
@@ -17,12 +25,24 @@ function Navbar({ downloadJson, uploadJson, characterList, currentCharacter, set
                   </li>
                 )
               })}
-            </div>
-          <div className='new-character-button' onClick={() => createNewCharacter()}>
+            <div className='new-character-button' onClick={() => createNewCharacter()}>
               <div>+</div>
             </div>
+            </div>
+          <div className={`help-button ${!hasViewedHelp && 'help-button-first-open'}`} onClick={() => { setModalOpen(true); setViewedHelp() }}>?</div>
           </ul>
+          
         </nav>
+        { modalOpen && <CustomModal className="instruction-modal" closeModal={() => {setModalOpen(false)}}>
+          <h1 className="instruction-text">Mission Briefing</h1>
+          <p className="instruction-text">How to use this character sheet:</p>
+          <div className='instruction-text instruction-body'>
+            <h2 className="instruction-text">Create or upload a new character:</h2>
+            <p className="instruction-text">Use the + button on the top left of the screen. You can name your new character by filling out the codename field on the sheet</p>
+            <h2 className="instruction-text">Save or share a character:</h2>
+            <p className="instruction-text">Characters are saved automatically in browser local storage. If you want to download them for safe keeping or sharing for using on another computer, you can use the download button in the top corner of the character sheet.</p>
+          </div>
+        </CustomModal>}
     </div>
   );
 }
